939ab2a396346e6fdddd557ec8901f3b64bd1eed
Nidia Guadarrama <47833881+Aly218@users.noreply.github.com>
2023-11-23 18:22:24 -0600

    updating pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -5,4 +11,16 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+
+function validateForm() {
+  let x = document.forms["myForm"]["lname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+	else{
+		alert("Thank you for signing up!");
+	}
 }
